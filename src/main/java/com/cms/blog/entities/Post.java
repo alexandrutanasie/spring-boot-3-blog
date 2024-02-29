9d124bf120db9ded1bf95af445c678e8a9dbd89d
Alexandru Tanasie <alexandru.tanasie@playmore.ro>
2024-02-29 16:16:21 +0200

    add url and created by for post

--- src/main/java/com/cms/blog/entities/Post.java
+++ src/main/java/com/cms/blog/entities/Post.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.cms.blog.components.AppComponent;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,11 +16,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import lombok.Data;
 
 @Data
 @Entity
 public class Post {
+    private AppComponent appComponent;
+
+    public Post(AppComponent appComponent){
+        this.appComponent = appComponent;
+    }
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,14 +40,25 @@ public class Post {
     @Column(name = "message", columnDefinition = "TEXT", nullable = true)
     private String content;
 
-    @CreationTimestamp
-    private LocalDateTime createdAt;
+    private String url;
 
     private String status;
+
+    @ManyToOne
+    private SiteUser createdBy;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     @ManyToMany
     @JoinTable(name = "post_category",
             joinColumns = @JoinColumn(name = "post_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     List<Category> categories = new ArrayList<>();
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdBy = appComponent.getCurrentUser();
+    }
 }
