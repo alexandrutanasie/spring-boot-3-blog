0729ea63f7ec3fecce325ec35cef374f2e490c5f
Alexandru Tanasie <alexandrutanasiee@gmail.com>
2024-03-02 13:57:24 +0200

    use layout for navbar

--- src/main/java/com/cms/blog/controllers/AppControllerAdvice.java
+++ src/main/java/com/cms/blog/controllers/AppControllerAdvice.java
@@ -1,19 +1,29 @@
 package com.cms.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.cms.blog.components.AppComponent;
+import com.cms.blog.entities.Category;
 import com.cms.blog.entities.SiteUser;
+import com.cms.blog.services.CategoryService;
 
 @ControllerAdvice
 public class AppControllerAdvice {
     @Autowired
     private AppComponent appComponent;
+    @Autowired
+    private CategoryService categoryService;
 
     @ModelAttribute("currentUser")
     public SiteUser getCurrentUser() {
         return appComponent.getCurrentUser();
+    }
+    @ModelAttribute("categories")
+    public List<Category> getCategories() {
+        return categoryService.getAllCategories();
     }
 }
--- src/main/java/com/cms/blog/controllers/FrontendController.java
+++ src/main/java/com/cms/blog/controllers/FrontendController.java
@@ -31,23 +31,19 @@ public class FrontendController {
 
     @GetMapping("/")
     public String index(Model model) {
-        List<Category> categories = categoryService.getAllCategories();
         List<Post> latestPosts = postService.findTop6ByOrderByCreatedAtDesc();
         
         model.addAttribute("title", "Homepage");
         model.addAttribute("latestPosts", latestPosts);
-        model.addAttribute("categories", categories);
         return "index";
     }
 
     @GetMapping("/category/{url}")
     public String getCategoryPage(@PathVariable("url") String url, Model model) {
         Optional<Category> _category = categoryService.findByUrl(url);
-        List<Category> categories = categoryService.getAllCategories();
         if(_category.isPresent()){
            Category category = _category.get();
             model.addAttribute("category", category);
-            model.addAttribute("categories", categories);
         }else{
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
@@ -56,12 +52,10 @@ public class FrontendController {
 
     @GetMapping("/post/{url}")
     public String getPostPage(@PathVariable("url") String url, Model model) {
-        List<Category> categories = categoryService.getAllCategories();
         Optional<Post> _post = postService.findByUrl(url);
         if(_post.isPresent()){
            Post post = _post.get();
             model.addAttribute("post", post);
-            model.addAttribute("categories", categories);
         }else{
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
